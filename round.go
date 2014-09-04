acf642333a85e67bace2a3bc6305db47c0c6c91a
Andrew M Bursavich <abursavich@gmail.com>
2014-09-03 18:26:38 -0700

    uint64

--- round.go
+++ round.go
@@ -13,7 +13,7 @@ const (
 	second = int64(time.Second)
 )
 
-var pow10tab [19]int64
+var pow10tab [20]uint64
 
 func init() {
 	pow10tab[0] = 1
@@ -22,15 +22,15 @@ func init() {
 	}
 }
 
-// Duration returns the result of rounding d to the nearest multiple of m.
-// The rounding behavior for halfway values is to round up. If m <= 0,
+// Duration returns the result of rounding d to the nearest multiple of n.
+// The rounding behavior for halfway values is to round up. If n <= 0,
 // Round returns d unchanged.
-func Duration(d, m time.Duration) time.Duration {
-	return time.Duration(Int64(int64(d), int64(m)))
+func Duration(d, n time.Duration) time.Duration {
+	return time.Duration(Int64(int64(d), int64(n)))
 }
 
-// DurationN returns the result of rounding d to n significant figures for
-// standard string formatting. The rounding behavior for halfway values
+// DurationN returns the result of rounding d to n significant decimal figures
+// for standard string formatting. The rounding behavior for halfway values
 // is to round up. If n <= 0, DurationN returns d unchanged.
 //
 // Examples (representing time.Duration values as strings):
@@ -50,42 +50,42 @@ func DurationN(d time.Duration, n int) time.Duration {
 	}
 	v := int64(d)
 	if v >= hour {
-		k := digits(v / hour)
+		k := i64digits(v / hour)
 		if k >= n {
-			return time.Duration(Int64(v, pow10(hour, k-n)))
+			return time.Duration(Int64(v, i64pow10(hour, k-n)))
 		}
 		n -= k
-		k = digits(v % hour / minute)
+		k = i64digits(v % hour / minute)
 		if k >= n {
-			return time.Duration(Int64(v, pow10(minute, k-n)))
+			return time.Duration(Int64(v, i64pow10(minute, k-n)))
 		}
-		return time.Duration(Int64(v, pow10(100*second, k-n)))
+		return time.Duration(Int64(v, i64pow10(100*second, k-n)))
 	}
 	if v >= minute {
-		k := digits(v / minute)
+		k := i64digits(v / minute)
 		if k >= n {
-			return time.Duration(Int64(v, pow10(minute, k-n)))
+			return time.Duration(Int64(v, i64pow10(minute, k-n)))
 		}
-		return time.Duration(Int64(v, pow10(100*second, k-n)))
+		return time.Duration(Int64(v, i64pow10(100*second, k-n)))
 	}
 	return time.Duration(Int64N(v, n))
 }
 
-// Int64 returns the result of rounding v to the nearest multiple of m.
-// The rounding behavior for halfway values is to round up. If m <= 0,
+// Int64 returns the result of rounding v to the nearest multiple of n.
+// The rounding behavior for halfway values is to round up. If n <= 0,
 // Int64 returns v unchanged.
-func Int64(v, m int64) int64 {
-	if m <= 0 {
+func Int64(v, n int64) int64 {
+	if n <= 0 {
 		return v
 	}
 	neg := v < 0
 	if neg {
 		v = -v
 	}
-	if r := v % m; r+r < m {
+	if r := v % n; r+r < n {
 		v = v - r
 	} else {
-		v = v + m - r
+		v = v + n - r
 	}
 	if neg {
 		return -v
@@ -93,23 +93,52 @@ func Int64(v, m int64) int64 {
 	return v
 }
 
-// Int64N returns the result of rounding v to n significant figures.
+// Int64N returns the result of rounding v to n significant decimal figures.
 // The rounding behavior for halfway values is to round up. If n <= 0,
 // Int64N returns v unchanged.
 func Int64N(v int64, n int) int64 {
 	if n <= 0 {
 		return v
 	}
-	if e := digits(v) - n; e > 0 {
-		return Int64(v, pow10(1, e))
+	if e := i64digits(v) - n; e > 0 {
+		return Int64(v, i64pow10(1, e))
 	}
 	return v
 }
 
-func digits(v int64) int {
+// Uint64 returns the result of rounding v to the nearest multiple of n.
+// The rounding behavior for halfway values is to round up. If n == 0,
+// Uint64 returns v unchanged.
+func Uint64(v, n uint64) uint64 {
+	if n == 0 {
+		return v
+	}
+	r := v % n
+	if r+r < n {
+		return v - r
+	}
+	return v + n - r
+}
+
+// Uint64N returns the result of rounding v to n significant decimal figures.
+// The rounding behavior for halfway values is to round up. If n == 0,
+// Uint64N returns v unchanged.
+func Uint64N(v uint64, n int) uint64 {
+	if n == 0 {
+		return v
+	}
+	if e := u64digits(v) - n; e > 0 {
+		return Uint64(v, u64pow10(1, e))
+	}
+	return v
+}
+
+// return range: [1,19]
+func i64digits(v int64) int {
 	if v < 0 {
 		v = -v
 	}
+	// return u64digits(uint64(v))
 	d := 1
 	for v > 9 {
 		v /= 10
@@ -118,7 +147,26 @@ func digits(v int64) int {
 	return d
 }
 
-func pow10(b int64, e int) int64 {
+// return range: [1,20]
+func u64digits(v uint64) int {
+	d := 1
+	for v > 9 {
+		v /= 10
+		d++
+	}
+	return d
+}
+
+// e range: [-19,19]
+func i64pow10(b int64, e int) int64 {
+	if e < 0 {
+		return b / int64(pow10tab[-e])
+	}
+	return b * int64(pow10tab[e])
+}
+
+// e range: [-20,20]
+func u64pow10(b uint64, e int) uint64 {
 	if e < 0 {
 		return b / pow10tab[-e]
 	}
